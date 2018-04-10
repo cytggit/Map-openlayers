0ed9731425950f6fd42620a181fc19b6bd4415ec
cyt <178462289@qq.com>
2018-04-10 14:57:19 +0800

    更改style显示优先度

--- js/Common/ol3-style.js
+++ js/Common/ol3-style.js
@@ -611,7 +611,7 @@ var geojsonstyle = {
 		fill: new ol.style.Fill({
 			color: [255,153,204,0.5]
 		}),
-		zIndex:102
+		zIndex:103
 	}),
 	'10030603' /*沙发*/: new ol.style.Style({  
 		stroke: new ol.style.Stroke({
@@ -691,7 +691,7 @@ var geojsonstyle = {
 		fill: new ol.style.Fill({
 			color: [244,244,204,0.8]
 		}),
-		zIndex: 103
+		zIndex: 105
 	}),
 
 
