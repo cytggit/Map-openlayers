9e07874fd86f4a1a7b8bbdb5e96ab343fc1d4a2c
cyt <178462289@qq.com>
2019-09-02 16:12:52 +0800

    3d

--- js/Cesium/Source/ThirdParty/GltfPipeline/addToArray.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-    'use strict';
-
-    function addToArray(array, element) {
-        array.push(element);
-        return array.length - 1;
-    }
-    return addToArray;
-});
